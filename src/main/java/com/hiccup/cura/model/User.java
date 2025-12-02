c0438e7023ff3f41fde356541c629db6292dcc4c
Hiccup05 <paudelbipin05@gmail.com>
2025-12-02 20:05:56 +0545

    Create user model

---
+++ src/main/java/com/hiccup/cura/model/User.java
@@ -0,0 +1,34 @@
+package com.hiccup.cura.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@Table(name="Users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    private Long id;
+    @Column(name="first_name")
+    private String firstName;
+    @Column(name="middle_name")
+    private String middleName;
+    @Column(name="last_name")
+    private String lastName;
+    private String email;
+    private String password;
+    @Column(name="mob_no")
+    private String mobNo;
+    @Column(name="date_of_birth")
+    private Date dateOfBirth;
+    private String address;
+}
