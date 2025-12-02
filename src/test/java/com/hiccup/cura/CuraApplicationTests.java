7fe5b997822b088f0982e4a7c2d1d8426ee7ad9d
Hiccup05 <paudelbipin05@gmail.com>
2025-12-02 19:56:13 +0545

    Setup environemnt

---
+++ src/test/java/com/hiccup/cura/CuraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hiccup.cura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
