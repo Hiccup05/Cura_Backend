3bb2eebe0701ec1ffcfa77609a265a8facc1f00f
Hiccup05 <paudelbipin05@gmail.com>
2025-12-07 20:47:05 +0545

    create UserLoginResponseDto

--- src/main/java/com/hiccup/cura/response/UserLoginResponseDto.java
+++ src/main/java/com/hiccup/cura/response/UserLoginResponseDto.java
@@ -1,4 +1,15 @@
 package com.hiccup.cura.response;
 
+import jakarta.persistence.Column;
+
+import java.util.Date;
+
 public class UserLoginResponseDto {
+    private Long id;
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private String mobNo;
+    private Date dateOfBirth;
+    private String address;
 }
