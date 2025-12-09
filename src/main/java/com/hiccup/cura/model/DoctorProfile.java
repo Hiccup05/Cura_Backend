3c3e7adf65bf7ee6177c9859d2788d3557974aed
Hiccup05 <paudelbipin05@gmail.com>
2025-12-09 20:51:50 +0545

    create attributes of doctor profile model

--- src/main/java/com/hiccup/cura/model/DoctorProfile.java
+++ src/main/java/com/hiccup/cura/model/DoctorProfile.java
@@ -1,7 +1,23 @@
 package com.hiccup.cura.model;
 
-import jakarta.persistence.Entity;
+import com.hiccup.cura.enums.DoctorStatus;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name="doctor_profile")
 public class DoctorProfile {
+    private Long id;
+    private String specialization;
+    @Enumerated(EnumType.STRING)
+    private DoctorStatus doctorStatus;
+    private String licenseNumber;
+    private String experience;
+    @JoinColumn(name="user_id")
+    private User user;
 }
