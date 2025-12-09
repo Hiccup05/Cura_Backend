53ca075d5db923c07879740ed71951c6ee447d0d
Hiccup05 <paudelbipin05@gmail.com>
2025-12-09 20:57:05 +0545

    add attributes for role model

--- src/main/java/com/hiccup/cura/model/Role.java
+++ src/main/java/com/hiccup/cura/model/Role.java
@@ -1,4 +1,18 @@
 package com.hiccup.cura.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
 public class Role {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private int id;
+    @Enumerated(EnumType.STRING)
+    private Role role;
 }
