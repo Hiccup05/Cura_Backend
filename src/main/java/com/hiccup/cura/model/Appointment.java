a105c4873f476c7b9efe3292d2b73f9fed0689bb
Hiccup05 <paudelbipin05@gmail.com>
2025-12-09 21:12:33 +0545

    add attributes in apppointment model

--- src/main/java/com/hiccup/cura/model/Appointment.java
+++ src/main/java/com/hiccup/cura/model/Appointment.java
@@ -1,6 +1,34 @@
 package com.hiccup.cura.model;
 
+import com.hiccup.cura.enums.AppointmentStatus;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
 public class Appointment {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Date createdAt;
+    private AppointmentStatus status;
+    private Date appointmentTime;
+    @JoinColumn(name="created_by")
+    @OneToMany
+    private User createdBy;
+    @OneToMany
+    @JoinColumn(name="doctor_id")
+    private DoctorProfile doctor;
+    @OneToMany
+    @JoinColumn(name="staff_id")
+    private StaffProfile staffProfile;
+    @OneToOne
+    @JoinColumn(name="product_id")
+    private Product product;
 }
