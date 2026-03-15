237242424573a1c98cf89c6a2f733e66f0ad766c
Hiccup05 <paudelbipin05Z@gmail.com>
2026-03-15 18:12:54 +0545

    create admin specialization controler

--- src/main/java/com/hiccup/cura/controller/admin/AdminSpecializationController.java
+++ src/main/java/com/hiccup/cura/controller/admin/AdminSpecializationController.java
@@ -1,4 +1,35 @@
 package com.hiccup.cura.controller.admin;
 
+import com.hiccup.cura.dto.reqeust.SpecializationRequestDto;
+import com.hiccup.cura.dto.response.MessageResponseDto;
+import com.hiccup.cura.model.Specialization;
+import com.hiccup.cura.service.doctor.specialization.SpecializationService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("${api.prefix}/admin/specialization")
+@RequiredArgsConstructor
 public class AdminSpecializationController {
+    private final SpecializationService service;
+
+    public ResponseEntity<Specialization> createSpecialization(@Valid @RequestBody  SpecializationRequestDto specializationRequestDto){
+        Specialization created =service.create(specializationRequestDto);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(created.getId())
+                .toUri();
+        return ResponseEntity.created(location).body(created);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<MessageResponseDto> createSpecialization(@PathVariable Long id){
+        return ResponseEntity.ok(service.delete(id));
+    }
 }
