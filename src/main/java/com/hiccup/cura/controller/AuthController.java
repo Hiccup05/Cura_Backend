2783dbec9c74e368e90906b29b987bd4dbf1442a
Hiccup05 <paudelbipin05Z@gmail.com>
2026-03-15 20:31:53 +0545

    create auth controller and local login endpoint

--- src/main/java/com/hiccup/cura/controller/AuthController.java
+++ src/main/java/com/hiccup/cura/controller/AuthController.java
@@ -1,13 +1,23 @@
 package com.hiccup.cura.controller;
 
+import com.hiccup.cura.dto.reqeust.LoginRequestDto;
+import com.hiccup.cura.dto.response.LoginResponseDto;
 import com.hiccup.cura.security.AuthService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("${api.prefix}/auth/")
+@RequestMapping("${api.prefix}/auth")
 public class AuthController {
     private final AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDto> localLogin(@RequestBody LoginRequestDto requestDto){
+        return ResponseEntity.accepted().body(authService.localLogin(requestDto));
+    }
 }
