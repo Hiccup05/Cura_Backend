30ed8e74d27de8bfa38438013c7ec613ea6dc9bc
Hiccup05 <paudelbipin05Z@gmail.com>
2026-03-16 15:39:54 +0545

    change repository naming

--- src/main/java/com/hiccup/cura/repository/MedicalServiceRepository.java
+++ src/main/java/com/hiccup/cura/repository/MedicalServiceRepository.java
@@ -1,6 +1,6 @@
 package com.hiccup.cura.repository;
 
-import com.hiccup.cura.model.Service;
+import com.hiccup.cura.model.MedicalService;
 import com.hiccup.cura.model.Specialization;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface MedicalServiceRepository extends JpaRepository<Service, Long> {
-    List<Service> findAllByIsActiveTrue();
-    List<Service> findBySpecializationAndIsActiveTrue(Specialization specialization);
+public interface MedicalServiceRepository extends JpaRepository<MedicalService, Long> {
+    List<MedicalService> findAllByIsActiveTrue();
+    List<MedicalService> findAllBySpecializationAndIsActiveTrue(Specialization specialization);
 }
--- src/main/java/com/hiccup/cura/service/MedicalService/MedicalServiceService.java
+++ src/main/java/com/hiccup/cura/service/MedicalService/MedicalServiceService.java
@@ -1,4 +1,34 @@
 package com.hiccup.cura.service.MedicalService;
 
+import com.hiccup.cura.dto.response.MedicalServiceResponseDto;
+import com.hiccup.cura.model.MedicalService;
+import com.hiccup.cura.model.Specialization;
+import com.hiccup.cura.repository.MedicalServiceRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class MedicalServiceService {
+   private final MedicalServiceRepository medicalServiceRepository;
+
+   public List<MedicalServiceResponseDto> getAll(){
+       return medicalServiceRepository.findAll().stream().map(this::mapToDto).toList();
+   }
+
+   public List<MedicalServiceResponseDto> getActiveServices(){
+       return medicalServiceRepository.findAllByIsActiveTrue().stream().map(this::mapToDto).toList();
+   }
+
+    public List<MedicalServiceResponseDto> getActiveSpecializationServices(Specialization specialization){
+        return medicalServiceRepository.findAllBySpecializationAndIsActiveTrue(specialization).stream().map(this::mapToDto).toList();
+    }
+
+   private MedicalServiceResponseDto mapToDto(MedicalService service){
+       return new MedicalServiceResponseDto(
+               service.getName(), service.getPrice(), service.getDurationMinutes(),
+                       service.getDescription(), service.getIsActive(),service.getSpecialization());
+   }
 }
