1499988f38586bb27726960b3efafd4b810ae4bb
Hiccup05 <paudelbipin05Z@gmail.com>
2026-03-16 15:04:06 +0545

    create medical service entity

--- src/main/java/com/hiccup/cura/model/Service.java
+++ src/main/java/com/hiccup/cura/model/Service.java
@@ -1,4 +1,26 @@
 package com.hiccup.cura.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Getter
+@Setter
 public class Service {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private BigDecimal price;
+    private Integer durationMinutes;
+    private String description;
+    private Boolean isActive;
+
+    @ManyToOne
+    @JoinColumn(name = "specialization_id")
+    private Specialization specialization;
 }
