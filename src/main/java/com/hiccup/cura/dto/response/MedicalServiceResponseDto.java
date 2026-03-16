c785259e13b9e268ecc4f3e5a565c65bee4143d5
Hiccup05 <paudelbipin05Z@gmail.com>
2026-03-16 15:39:02 +0545

    create medical service dto

--- src/main/java/com/hiccup/cura/dto/response/MedicalServiceResponseDto.java
+++ src/main/java/com/hiccup/cura/dto/response/MedicalServiceResponseDto.java
@@ -1,4 +1,19 @@
 package com.hiccup.cura.dto.response;
+import com.hiccup.cura.model.Specialization;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class MedicalServiceResponseDto {
+    private String name;
+    private BigDecimal price;
+    private Integer durationMinutes;
+    private String description;
+    private Boolean isActive;
+    private Specialization specialization;
 }
