2428dc2592488d62dd0172a0e5e0dbaf880058ce
Hiccup05 <paudelbipin05Z@gmail.com>
2026-03-16 18:01:24 +0545

    create endpoint to get service from id

--- src/main/java/com/hiccup/cura/controller/admin/AdminServiceController.java
+++ src/main/java/com/hiccup/cura/controller/admin/AdminServiceController.java
@@ -5,6 +5,7 @@ import com.hiccup.cura.service.medicalservice.MedicalServiceService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +20,10 @@ public class AdminServiceController {
     @GetMapping
     public ResponseEntity<List<MedicalServiceResponseDto>> getMedicalServices(){
         return ResponseEntity.ok(serviceService.getAll());
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<MedicalServiceResponseDto> getMedicalService(@PathVariable Long id){
+        return ResponseEntity.ok(serviceService.getService(id));
     }
 }
