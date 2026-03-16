ae67780e9c3ef51d1b0b598823247826ae561f9f
Hiccup05 <paudelbipin05Z@gmail.com>
2026-03-16 18:37:03 +0545

    create get active service endpoint

--- src/main/java/com/hiccup/cura/controller/ServiceController.java
+++ src/main/java/com/hiccup/cura/controller/ServiceController.java
@@ -1,12 +1,26 @@
 package com.hiccup.cura.controller;
 
+import com.hiccup.cura.dto.response.MedicalServiceResponseDto;
+import com.hiccup.cura.repository.MedicalServiceRepository;
+import com.hiccup.cura.service.medicalservice.MedicalServiceService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("${api.prefix}/service")
 @RequiredArgsConstructor
 public class ServiceController {
+    private final MedicalServiceService serviceService;
 
+    @GetMapping
+    public RequestEntity<List<MedicalServiceResponseDto>> getAllServices(){
+        return ResponseEntity.ok(serviceService.getActiveServices());
+    }
 }
