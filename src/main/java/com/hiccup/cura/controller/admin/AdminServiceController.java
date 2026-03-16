3dbd8a76bd2658667ebfd6c10aa0a6f3a492382a
Hiccup05 <paudelbipin05Z@gmail.com>
2026-03-16 18:10:39 +0545

    create admin service update and delete endponits

--- src/main/java/com/hiccup/cura/controller/admin/AdminServiceController.java
+++ src/main/java/com/hiccup/cura/controller/admin/AdminServiceController.java
@@ -1,14 +1,15 @@
 package com.hiccup.cura.controller.admin;
 
+import com.hiccup.cura.dto.reqeust.MedicalServiceRequestDto;
 import com.hiccup.cura.dto.response.MedicalServiceResponseDto;
+import com.hiccup.cura.dto.response.MessageResponseDto;
 import com.hiccup.cura.service.medicalservice.MedicalServiceService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -22,8 +23,29 @@ public class AdminServiceController {
         return ResponseEntity.ok(serviceService.getAll());
     }
 
-    @GetMapping("{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<MedicalServiceResponseDto> getMedicalService(@PathVariable Long id){
         return ResponseEntity.ok(serviceService.getService(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<MedicalServiceResponseDto> createMedicalService(@RequestBody MedicalServiceRequestDto medicalServiceRequestDto){
+        MedicalServiceResponseDto created=serviceService.createMedicalService(medicalServiceRequestDto);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(created.getId())
+                .toUri();
+        return ResponseEntity.created(location).body(created);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<MedicalServiceResponseDto> updateMedicalService(@PathVariable Long id, @RequestBody MedicalServiceRequestDto medicalServiceRequestDto){
+        return ResponseEntity.ok(serviceService.updateMedicalService(id, medicalServiceRequestDto));
+    }
+
+    @PatchMapping("/{id}/status")
+    public ResponseEntity<MessageResponseDto> toggleStatus(@PathVariable Long id){
+        return ResponseEntity.ok(serviceService.toggleStatus(id));
     }
 }
