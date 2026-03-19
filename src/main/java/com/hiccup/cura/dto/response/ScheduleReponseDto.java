0a0d34849f9ba6e234332fc404aecb03f2fe08af
Hiccup05 <paudelbipin05Z@gmail.com>
2026-03-19 20:40:59 +0545

    create doctor schedule response dto

--- src/main/java/com/hiccup/cura/dto/response/ScheduleReponseDto.java
+++ src/main/java/com/hiccup/cura/dto/response/ScheduleReponseDto.java
@@ -1,4 +1,24 @@
 package com.hiccup.cura.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class ScheduleReponseDto {
+    private Long id;
+    private String doctorName;
+    private Long doctorId;
+    private DayOfWeek dayOfWeek;
+    private LocalTime startTime;
+    private LocalTime endTime;
+    private Integer maxAppointments;
+    private Boolean isAvailable;
 }
