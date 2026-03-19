a28511c279a1f2d360301cd1b665926b68b1d113
Hiccup05 <paudelbipin05Z@gmail.com>
2026-03-19 20:33:07 +0545

    create doctor schedule entity

---
+++ src/main/java/com/hiccup/cura/model/DoctorSchedule.java
@@ -0,0 +1,29 @@
+package com.hiccup.cura.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+
+@Entity
+@Getter
+@Setter
+public class DoctorSchedule {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "doctor_id")
+    private DoctorProfile doctor;
+
+    @Enumerated(EnumType.STRING)
+    private DayOfWeek dayOfWeek;
+
+    private LocalTime startTime;
+    private LocalTime endTime;
+    private Integer maxAppointments;
+    private Boolean isAvailable;
+}
---
+++ src/main/java/com/hiccup/cura/service/DoctorScheduleService.java
@@ -0,0 +1,7 @@
+package com.hiccup.cura.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DoctorScheduleService {
+}
