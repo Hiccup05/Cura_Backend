01ddad95f0d6e57ffe11ba3d2a22aff03d768d00
Hiccup05 <paudelbipin05Z@gmail.com>
2026-03-20 11:59:24 +0545

    create doctor controller

--- src/main/java/com/hiccup/cura/controller/DoctorController.java
+++ src/main/java/com/hiccup/cura/controller/DoctorController.java
@@ -1,4 +1,19 @@
 package com.hiccup.cura.controller;
 
+import com.hiccup.cura.service.DoctorScheduleService;
+import com.hiccup.cura.service.doctor.DoctorService;
+import com.hiccup.cura.service.doctor.specialization.SpecializationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("${api.prefix}/doctors")
+@RequiredArgsConstructor
 public class DoctorController {
+    private final DoctorService doctorService;
+    private final DoctorScheduleService doctorScheduleService;
+    private final SpecializationService specializationService;
+
+
 }
