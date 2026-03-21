5757b5b6abaf33bb8363318be67b307bbaa5ef41
Hiccup05 <paudelbipin05Z@gmail.com>
2026-03-21 16:47:46 +0545

    create java time  module for jackson

--- src/main/java/com/hiccup/cura/config/Utility.java
+++ src/main/java/com/hiccup/cura/config/Utility.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import tools.jackson.databind.ObjectMapper;
+import tools.jackson.databind.json.JsonMapper;
 
 @Configuration
 public class Utility {
@@ -17,5 +18,12 @@ public class Utility {
     @Bean
     public ModelMapper modelMapper(){
         return new ModelMapper();
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return JsonMapper.builder()
+                .addModule(new JavaTimeModule())
+                .build();
     }
 }
