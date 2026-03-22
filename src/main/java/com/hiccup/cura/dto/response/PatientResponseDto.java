f6b577fbb61e4fb8573a05c42a0c302ea2964337
Hiccup05 <paudelbipin05Z@gmail.com>
2026-03-22 14:17:19 +0545

    create patient response dto

---
+++ src/main/java/com/hiccup/cura/dto/response/PatientResponseDto.java
@@ -0,0 +1,41 @@
+package com.hiccup.cura.dto.response;
+
+import com.hiccup.cura.enums.BloodGroup;
+import com.nimbusds.openid.connect.sdk.claims.Gender;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class PatientResponseDto {
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    private LocalDate dateOfBirth;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    private String phoneNumber;
+    private String address;
+
+    @Enumerated(EnumType.STRING)
+    private BloodGroup bloodGroup;
+
+    private String allergies;
+
+    private String chronicConditions;
+
+    private String emergencyContactName;
+
+    private String emergencyContactPhone;
+}
