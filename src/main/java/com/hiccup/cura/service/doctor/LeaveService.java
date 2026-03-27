f42bcdcfdf10ef16feda6c3268353300186ee03c
Hiccup05 <paudelbipin05Z@gmail.com>
2026-03-27 13:50:53 +0545

    Create doctor leave service

--- src/main/java/com/hiccup/cura/service/doctor/LeaveService.java
+++ src/main/java/com/hiccup/cura/service/doctor/LeaveService.java
@@ -1,4 +1,15 @@
 package com.hiccup.cura.service.doctor;
 
-public class LeaveServoice {
+import com.hiccup.cura.repository.DoctorLeaveRepository;
+import com.hiccup.cura.repository.DoctorRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LeaveService {
+    private final DoctorLeaveRepository doctorLeaveRepository;
+    private final DoctorRepository doctorRepository;
+
+    
 }
