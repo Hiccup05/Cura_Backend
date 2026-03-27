3814f2f1ca91d9cae04d32e7cbba5b139cacefc6
Hiccup05 <paudelbipin05Z@gmail.com>
2026-03-27 17:20:55 +0545

    update get schedule endpoint

--- src/main/java/com/hiccup/cura/controller/publics/PublicDoctorController.java
+++ src/main/java/com/hiccup/cura/controller/publics/PublicDoctorController.java
@@ -1,5 +1,6 @@
 package com.hiccup.cura.controller.publics;
 
+import com.hiccup.cura.dto.response.PublicScheduleResponseDto;
 import com.hiccup.cura.dto.response.ScheduleResponseDto;
 import com.hiccup.cura.service.DoctorScheduleService;
 import com.hiccup.cura.service.doctor.DoctorService;
@@ -19,7 +20,7 @@ public class PublicDoctorController {
     private final SpecializationService specializationService;
 
     @GetMapping("/{id}/schedule")
-    public ResponseEntity<List<ScheduleResponseDto>> getDoctorScheduleByDoctorProfileId(@PathVariable Long id) {
-        return ResponseEntity.ok(doctorScheduleService.getSchedulesOfDoctor(id));
+    public ResponseEntity<List<PublicScheduleResponseDto>> getDoctorScheduleByDoctorProfileId(@PathVariable Long id) {
+        return ResponseEntity.ok(doctorScheduleService.getPublicSchedulesOfDoctor(id));
     }
 }
