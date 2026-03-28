475aa62a40ba77c41165da78859b2d00cc022a11
Hiccup05 <paudelbipin05Z@gmail.com>
2026-03-28 16:39:08 +0545

    create receptionist profile

--- src/main/java/com/hiccup/cura/model/ReceptionistProfile.java
+++ src/main/java/com/hiccup/cura/model/ReceptionistProfile.java
@@ -1,4 +1,29 @@
 package com.hiccup.cura.model;
 
+import com.hiccup.cura.enums.ReceptionistStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class ReceptionistProfile {
+    @Id
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+
+    @Enumerated(EnumType.STRING)
+    private ReceptionistStatus status;
+
+    @MapsId
+    @OneToOne
+    private User user;
 }
