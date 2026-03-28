b806c07589fe8c809209862f498301e312c5df4a
Hiccup05 <paudelbipin05Z@gmail.com>
2026-03-28 17:42:14 +0545

    create get receptionists endpoint

--- src/main/java/com/hiccup/cura/controller/admin/AdminReceptionistController.java
+++ src/main/java/com/hiccup/cura/controller/admin/AdminReceptionistController.java
@@ -1,8 +1,32 @@
 package com.hiccup.cura.controller.admin;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.hiccup.cura.dto.reqeust.ChangeReceptionistRequestDto;
+import com.hiccup.cura.dto.reqeust.ReceptionistRequestDto;
+import com.hiccup.cura.dto.response.ReceptionistResponseDto;
+import com.hiccup.cura.service.ReceptionistService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
 
 @RestController
-@Request
+@RequestMapping("${api.prefix}/admin/receptionists")
+@RequiredArgsConstructor
 public class AdminReceptionistController {
+    private final ReceptionistService receptionistService;
+
+    @GetMapping
+    public ResponseEntity<List<ReceptionistResponseDto>> getReceptionists() {
+        return ResponseEntity.ok(receptionistService.getReceptionists());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ReceptionistResponseDto> getReceptionist(@PathVariable Long id) {
+        return ResponseEntity.ok(receptionistService.getReceptionist(id));
+    }
+
+
 }
