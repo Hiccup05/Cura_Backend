82096ab2572a160245ea9c6da16a7be39aa42b58
Hiccup05 <paudelbipin05Z@gmail.com>
2026-03-28 17:29:59 +0545

    create receptionist service

--- src/main/java/com/hiccup/cura/service/ReceptionistService.java
+++ src/main/java/com/hiccup/cura/service/ReceptionistService.java
@@ -1,4 +1,36 @@
 package com.hiccup.cura.service;
 
+import com.hiccup.cura.dto.reqeust.ReceptionistRequestDto;
+import com.hiccup.cura.dto.response.ReceptionistResponseDto;
+import com.hiccup.cura.enums.RoleType;
+import com.hiccup.cura.exception.custom.DuplicateEntryException;
+import com.hiccup.cura.exception.custom.ResourceNotFoundException;
+import com.hiccup.cura.model.ReceptionistProfile;
+import com.hiccup.cura.model.User;
+import com.hiccup.cura.repository.ReceptionistRepository;
+import com.hiccup.cura.repository.RoleRepository;
+import com.hiccup.cura.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Set;
+
+@Service
+@RequiredArgsConstructor
 public class ReceptionistService {
+    private final ReceptionistRepository receptionistRepository;
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+
+
+    private ReceptionistResponseDto mapToDto(ReceptionistProfile profile) {
+        return ReceptionistResponseDto.builder()
+                .id(profile.getId())
+                .firstName(profile.getFirstName())
+                .lastName(profile.getLastName())
+                .phoneNumber(profile.getPhoneNumber())
+                .build();
+    }
 }
