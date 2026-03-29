8dfef3ecf131aaaf989d4baf9c944d5d8711621a
Hiccup05 <paudelbipin05Z@gmail.com>
2026-03-29 08:06:34 +0545

    complete appointment entity

--- src/main/java/com/hiccup/cura/model/Appointment.java
+++ src/main/java/com/hiccup/cura/model/Appointment.java
@@ -1,4 +1,60 @@
 package com.hiccup.cura.model;
 
+import com.hiccup.cura.enums.AppointmentStatus;
+import com.hiccup.cura.enums.AppointmentType;
+import com.hiccup.cura.enums.PaymentMethod;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Appointment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "patient_id")
+    private PatientProfile patient;
+
+    @ManyToOne
+    @JoinColumn(name = "doctor_id")
+    private DoctorProfile doctor;
+
+    @ManyToOne
+    @JoinColumn(name = "receptionist_id")
+    private ReceptionistProfile receptionist;
+
+    @ManyToOne
+    @JoinColumn(name = "service_id")
+    private MedicalService medicalService;
+
+    private LocalDate appointmentDate;
+    private LocalTime appointmentTime;
+
+    @Enumerated(EnumType.STRING)
+    private AppointmentStatus status;
+
+    @Enumerated(EnumType.STRING)
+    private AppointmentType type;
+
+    private String reason;
+    private String doctorNotes;
+
+    private String walkInPatientName;
+    private String walkInPatientPhone;
+
+    private Boolean isPaid;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentMethod paymentMethod;
+
+    private LocalDateTime bookedAt;
 }
