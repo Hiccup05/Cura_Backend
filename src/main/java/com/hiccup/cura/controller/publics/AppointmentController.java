c0aa7de46ef64bae644dad839a477cf398a64028
Hiccup05 <paudelbipin05Z@gmail.com>
2026-03-29 19:30:40 +0545

    add create appointment endpoint

--- src/main/java/com/hiccup/cura/controller/publics/AppointmentController.java
+++ src/main/java/com/hiccup/cura/controller/publics/AppointmentController.java
@@ -1,14 +1,37 @@
 package com.hiccup.cura.controller.publics;
 
+import com.hiccup.cura.dto.reqeust.AppointmentRequestDto;
+import com.hiccup.cura.dto.response.AppointmentResponseDto;
+import com.hiccup.cura.model.User;
+import com.hiccup.cura.security.CustomUser;
 import com.hiccup.cura.service.AppointmentService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("${api.prefix}/appointment")
 public class AppointmentController {
     private final AppointmentService appointmentService;
+
+    @PostMapping
+    public ResponseEntity<AppointmentResponseDto> createAppointment(@RequestBody AppointmentRequestDto appointmentRequestDto, @AuthenticationPrincipal CustomUser user){
+        AppointmentResponseDto created = appointmentService.createAppointment(appointmentRequestDto, user.getId());
+        URI location= ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(created.getId())
+                .toUri();
+        return ResponseEntity.created(location).body(created);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AppointmentResponseDto> getAppointment(@PathVariable Long id, @AuthenticationPrincipal User user){
+        return null;
+    }
 }
