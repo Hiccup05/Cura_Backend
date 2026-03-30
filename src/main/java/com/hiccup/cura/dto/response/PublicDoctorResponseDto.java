638c364847af0cdff5ba4d7a51eb6a703b48bcbe
Hiccup05 <paudelbipin05Z@gmail.com>
2026-03-30 17:14:30 +0545

    create public doctor resposne dto

---
+++ src/main/java/com/hiccup/cura/dto/response/PublicDoctorResponseDto.java
@@ -0,0 +1,21 @@
+package com.hiccup.cura.dto.response;
+
+import com.hiccup.cura.enums.DoctorStatus;
+import com.hiccup.cura.model.Specialization;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PublicDoctorResponseDto {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private Set<Specialization> specialization;
+    private int  yearsOfExperience;
+    private String licenseNumber;
+    private DoctorStatus doctorStatus;
+}
