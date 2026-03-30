0c5c936db863782b98ebfe27dc9f76f2ddd0632b
Hiccup05 <paudelbipin05Z@gmail.com>
2026-03-30 17:26:40 +0545

    create get public doctor repository method

--- src/main/java/com/hiccup/cura/repository/DoctorRepository.java
+++ src/main/java/com/hiccup/cura/repository/DoctorRepository.java
@@ -1,11 +1,19 @@
 package com.hiccup.cura.repository;
 
+import com.hiccup.cura.enums.DoctorStatus;
 import com.hiccup.cura.model.DoctorProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface DoctorRepository extends JpaRepository<DoctorProfile, Long> {
 
     boolean existsById(Long id);
+
+    @Query("SELECT d FROM DoctorProfile d where d.doctorStatus IN :status")
+    List<DoctorProfile> getPublicDoctors(@Param("status") List<DoctorStatus> status);
 }
