c4da848454c05365c6ac299a345de4088114cbd8
Hiccup05 <paudelbipin05Z@gmail.com>
2026-03-30 16:47:35 +0545

    create get appointment by user id

--- src/main/java/com/hiccup/cura/repository/AppointmentRepository.java
+++ src/main/java/com/hiccup/cura/repository/AppointmentRepository.java
@@ -9,6 +9,7 @@ import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.List;
 import java.util.Optional;
 
 public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
@@ -22,4 +23,8 @@ public interface AppointmentRepository extends JpaRepository<Appointment, Long> 
     @Query("SELECT A FROM Appointment A WHERE "+
     " A.id=:id AND ((A.patient Is NOT NULL AND A.patient.id=:userId) OR (A.receptionist IS NOT NULL AND A.receptionist.id=:userId))")
     Optional<Appointment> getAppointmentByIdAndUserId(@Param("id")Long id,@Param("userId") Long userId);
+    @Query("Select A From Appointment A Where " +
+    "((A.patient Is NOT NULL AND A.patient.id=:userId) OR (A.receptionist IS NOT NULL AND A.receptionist.id=:userId))" +
+    " ORDER BY A.appointmentDate desc")
+    List<Appointment> getAppointmentOfUser(@Param("userId") Long userId);
 }
