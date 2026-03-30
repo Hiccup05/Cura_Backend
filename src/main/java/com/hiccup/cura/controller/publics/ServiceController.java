142e2e2bdfdcad9836f7a5dbd2dea35f5faa734c
Hiccup05 <paudelbipin05Z@gmail.com>
2026-03-30 09:22:09 +0545

    update request mapping endpoint

--- src/main/java/com/hiccup/cura/controller/publics/ServiceController.java
+++ src/main/java/com/hiccup/cura/controller/publics/ServiceController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("${api.prefix}/service")
+@RequestMapping("${api.prefix}/public/service")
 @RequiredArgsConstructor
 public class ServiceController {
     private final MedicalServiceService serviceService;
