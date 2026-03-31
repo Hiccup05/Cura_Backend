fd1998a96c766bce50639c49df444ab5eeb41420
Hiccup05 <paudelbipin05Z@gmail.com>
2026-03-31 14:26:11 +0545

    create prescription resposne dto

--- src/main/java/com/hiccup/cura/dto/response/PrescriptionResponseDto.java
+++ src/main/java/com/hiccup/cura/dto/response/PrescriptionResponseDto.java
@@ -1,4 +1,14 @@
 package com.hiccup.cura.dto.response;
 
-public class PrescriptionResonseDto {
+import com.hiccup.cura.model.Prescription;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PrescriptionResponseDto {
+    private Long id;
+    private String description;
 }
