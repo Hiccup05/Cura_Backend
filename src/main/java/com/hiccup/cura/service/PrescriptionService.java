ba47e57ff84c5f15c48e737ea8e2d97c63ac06e8
Hiccup05 <paudelbipin05Z@gmail.com>
2026-03-31 14:26:26 +0545

    create update prescription service method

--- src/main/java/com/hiccup/cura/service/PrescriptionService.java
+++ src/main/java/com/hiccup/cura/service/PrescriptionService.java
@@ -1,9 +1,35 @@
 package com.hiccup.cura.service;
 
+import com.hiccup.cura.dto.reqeust.PrescriptionRequestDto;
+import com.hiccup.cura.dto.response.PrescriptionResponseDto;
+import com.hiccup.cura.exception.custom.ResourceNotFoundException;
+import com.hiccup.cura.exception.custom.UnauthorizedUserAccessException;
+import com.hiccup.cura.model.Appointment;
+import com.hiccup.cura.model.Prescription;
+import com.hiccup.cura.repository.AppointmentRepository;
+import com.hiccup.cura.repository.PrescriptionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class PrescriptionService {
+    private final PrescriptionRepository prescriptionRepository;
+    private final AppointmentRepository appointmentRepository;
+
+    public PrescriptionResponseDto updatePrescription(PrescriptionRequestDto prescription,Long prescriptionId, Long doctorId) {
+        Appointment appointment = appointmentRepository.findById(prescriptionId).orElseThrow(() -> new ResourceNotFoundException("Appointment Not Found with id " + prescriptionId));
+        if(!appointment.getDoctor().getId().equals(doctorId)){
+            throw new UnauthorizedUserAccessException("Doctor Id dont match with the appointment id whose prescription is being updated");
+        }
+        Prescription saved = prescriptionRepository.findById(prescriptionId).orElseThrow(() -> new ResourceNotFoundException("Prescription not found with id " + prescriptionId));
+        if(prescription.getDescription()!=null && !prescription.getDescription().isBlank()){
+            saved.setDescription(prescription.getDescription());
+        }
+        return  mapToDto(prescriptionRepository.save(saved));
+    }
+
+    public PrescriptionResponseDto mapToDto(Prescription prescription){
+        return new  PrescriptionResponseDto(prescription.getId(), prescription.getDescription());
+    }
 }
