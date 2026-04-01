89daaefca883e00e0c1a8454a6442100fe8e847a
Hiccup05 <paudelbipin05Z@gmail.com>
2026-04-01 13:35:53 +0545

    create payment model

--- src/main/java/com/hiccup/cura/model/Payment.java
+++ src/main/java/com/hiccup/cura/model/Payment.java
@@ -1,4 +1,38 @@
 package com.hiccup.cura.model;
 
+import com.hiccup.cura.enums.PaymentStatus;
+import com.hiccup.cura.enums.PaymentType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private PaymentType paymentType;
+
+    private PaymentStatus paymentStatus;
+
+    private BigDecimal amount;
+
+    private String pidx;
+
+    private String transactionId;
+
+    private LocalDateTime paidAt;
+
+    @OneToOne
+    private Appointment appointment;
 }
