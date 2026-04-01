06c6cb5c115f555b420773cdb60695aeb29bd9ac
Hiccup05 <paudelbipin05Z@gmail.com>
2026-04-01 13:42:51 +0545

    create payment service

--- src/main/java/com/hiccup/cura/service/PaymentService.java
+++ src/main/java/com/hiccup/cura/service/PaymentService.java
@@ -1,4 +1,12 @@
 package com.hiccup.cura.service;
 
+import com.hiccup.cura.repository.AppointmentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
 public class PaymentService {
+    private final AppointmentRepository appointmentRepository;
+
 }
