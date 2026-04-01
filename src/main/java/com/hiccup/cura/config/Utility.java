83ec67243fadf814185027b21c450673d098ecc2
Hiccup05 <paudelbipin05Z@gmail.com>
2026-04-01 13:55:21 +0545

    add WebClient dependency

--- src/main/java/com/hiccup/cura/config/Utility.java
+++ src/main/java/com/hiccup/cura/config/Utility.java
@@ -3,6 +3,7 @@ package com.hiccup.cura.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
 import tools.jackson.databind.ObjectMapper;
 import tools.jackson.databind.json.JsonMapper;
 
@@ -12,6 +13,11 @@ public class Utility {
     @Bean
     public ModelMapper modelMapper(){
         return new ModelMapper();
+    }
+
+    @Bean
+    public WebClient webClient(){
+        return WebClient.create();
     }
 
     @Bean
