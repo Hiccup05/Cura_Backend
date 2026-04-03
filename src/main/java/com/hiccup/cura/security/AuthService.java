40e1663f8b9e4f6720a7fff7878a5c49df9445b8
Hiccup05 <paudelbipin05Z@gmail.com>
2026-04-03 19:18:35 +0545

    minor project update

--- src/main/java/com/hiccup/cura/security/AuthService.java
+++ src/main/java/com/hiccup/cura/security/AuthService.java
@@ -2,25 +2,13 @@ package com.hiccup.cura.security;
 
 import com.hiccup.cura.dto.reqeust.LoginRequestDto;
 import com.hiccup.cura.dto.response.LoginResponseDto;
-import com.hiccup.cura.dto.reqeust.SignUpRequestDto;
-import com.hiccup.cura.dto.response.SignupResponseDto;
-import com.hiccup.cura.enums.AuthType;
-import com.hiccup.cura.enums.RoleType;
-import com.hiccup.cura.model.User;
-import com.hiccup.cura.repository.RoleRepository;
-import com.hiccup.cura.repository.UserRepository;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
-import java.util.Set;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
