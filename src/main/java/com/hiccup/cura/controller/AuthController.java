5359c069ddbcd73aed953d7dccf00ee3b1241f97
Hiccup05 <paudelbipin05Z@gmail.com>
2026-04-03 19:17:43 +0545

    upadate local login to adapt cookie

--- src/main/java/com/hiccup/cura/controller/AuthController.java
+++ src/main/java/com/hiccup/cura/controller/AuthController.java
@@ -3,12 +3,20 @@ package com.hiccup.cura.controller;
 import com.hiccup.cura.dto.reqeust.LoginRequestDto;
 import com.hiccup.cura.dto.response.LoginResponseDto;
 import com.hiccup.cura.security.AuthService;
+import com.hiccup.cura.security.CustomUser;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.Nullable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RequiredArgsConstructor
 @RestController
@@ -17,7 +25,19 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/login")
-    public ResponseEntity<LoginResponseDto> localLogin(@RequestBody LoginRequestDto requestDto){
-        return ResponseEntity.accepted().body(authService.localLogin(requestDto));
+    public ResponseEntity<LoginResponseDto> localLogin(@RequestBody LoginRequestDto requestDto, HttpServletResponse response) {
+        LoginResponseDto loginResponseDto = authService.localLogin(requestDto);
+        Cookie cookie=new Cookie("token", loginResponseDto.getJwtToken());
+        cookie.setMaxAge(60*60);
+        cookie.setPath("/");
+        cookie.setHttpOnly(true);
+        response.addCookie(cookie);
+        return ResponseEntity.accepted().body(loginResponseDto);
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<Map<String, List<String>>> me(@AuthenticationPrincipal CustomUser user) {
+        Map<String, List<String>> role = Map.of("role", user.getAuthorities().stream().map(GrantedAuthority::getAuthority).toList());
+        return ResponseEntity.ok(role);
     }
 }
