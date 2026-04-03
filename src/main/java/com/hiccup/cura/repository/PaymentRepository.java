7b081ba4d8d98b7c37d7fff2005e3a2870d64ad5
Hiccup05 <paudelbipin05Z@gmail.com>
2026-04-03 19:15:24 +0545

    add find findByAppointment_IdAndPaymentStatusNotIn and findByPidx repository method

--- src/main/java/com/hiccup/cura/repository/PaymentRepository.java
+++ src/main/java/com/hiccup/cura/repository/PaymentRepository.java
@@ -1,4 +1,15 @@
 package com.hiccup.cura.repository;
 
-public class PaymentRepository {
+import com.hiccup.cura.enums.PaymentStatus;
+import com.hiccup.cura.model.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    Payment findByAppointment_IdAndPaymentStatusNotIn(Long appointmentId, List<PaymentStatus> paymentStatus);
+
+    Payment findByPidx(String pidx);
 }
