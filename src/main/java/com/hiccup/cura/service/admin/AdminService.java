e4cb8b380214205d103cebb9f483eb4a5ff3f415
Hiccup05 <paudelbipin05Z@gmail.com>
2026-04-04 08:51:26 +0545

    create getAdmin profile service

--- src/main/java/com/hiccup/cura/service/admin/AdminService.java
+++ src/main/java/com/hiccup/cura/service/admin/AdminService.java
@@ -1,5 +1,8 @@
 package com.hiccup.cura.service.admin;
 
+import com.hiccup.cura.dto.response.AdminProfileDto;
+import com.hiccup.cura.exception.custom.ResourceNotFoundException;
+import com.hiccup.cura.model.User;
 import com.hiccup.cura.repository.AppointmentRepository;
 import com.hiccup.cura.repository.DoctorRepository;
 import com.hiccup.cura.repository.UserRepository;
@@ -13,5 +16,13 @@ public class AdminService {
     private final DoctorRepository doctorRepository;
     private final AppointmentRepository appointmentRepository;
 
-
+    public AdminProfileDto getAdminProfile(Long userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("Admin not found with id " + userId));
+        return AdminProfileDto.builder()
+                .id(user.getId())
+                .username(user.getUsername())
+                .email(user.getEmail())
+                .build();
+    }
 }
