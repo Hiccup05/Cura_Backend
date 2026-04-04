4b6a5664ab8cfd9d159b9f7a9e0d4bd4f2253805
Hiccup05 <paudelbipin05Z@gmail.com>
2026-04-04 08:51:39 +0545

    create get admin endpoint

--- src/main/java/com/hiccup/cura/controller/admin/AdminController.java
+++ src/main/java/com/hiccup/cura/controller/admin/AdminController.java
@@ -1,4 +1,24 @@
 package com.hiccup.cura.controller.admin;
 
+import com.hiccup.cura.dto.response.AdminProfileDto;
+import com.hiccup.cura.model.User;
+import com.hiccup.cura.security.CustomUser;
+import com.hiccup.cura.service.admin.AdminService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("${api.prefix}/admin")
 public class AdminController {
+    private final AdminService adminService;
+
+    @GetMapping
+    public ResponseEntity<AdminProfileDto> getAdminProfile(@AuthenticationPrincipal CustomUser user) {
+        return ResponseEntity.ok(adminService.getAdminProfile(user.getId()));
+    }
 }
