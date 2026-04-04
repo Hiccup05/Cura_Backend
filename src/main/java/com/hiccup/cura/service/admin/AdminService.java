f14c87f6392a7f16bd368c080b94d5f63ce9a6c1
Hiccup05 <paudelbipin05Z@gmail.com>
2026-04-04 08:53:45 +0545

    add get all doctor

--- src/main/java/com/hiccup/cura/service/admin/AdminService.java
+++ src/main/java/com/hiccup/cura/service/admin/AdminService.java
@@ -1,13 +1,17 @@
 package com.hiccup.cura.service.admin;
 
 import com.hiccup.cura.dto.response.AdminProfileDto;
+import com.hiccup.cura.dto.response.DoctorDto;
 import com.hiccup.cura.exception.custom.ResourceNotFoundException;
+import com.hiccup.cura.model.DoctorProfile;
 import com.hiccup.cura.model.User;
 import com.hiccup.cura.repository.AppointmentRepository;
 import com.hiccup.cura.repository.DoctorRepository;
 import com.hiccup.cura.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -23,6 +27,22 @@ public class AdminService {
                 .id(user.getId())
                 .username(user.getUsername())
                 .email(user.getEmail())
+                .build();
+    }
+
+    public List<DoctorDto> getAllDoctors() {
+        return doctorRepository.findAll().stream()
+                .map(this::mapToDoctorDto).toList();
+    }
+
+
+    private DoctorDto mapToDoctorDto(DoctorProfile doctor) {
+        return DoctorDto.builder()
+                .id(doctor.getId())
+                .licenseNumber(doctor.getLicenseNumber())
+                .yearsOfExperience(doctor.getYearsOfExperience())
+                .specialization(doctor.getSpecialization())
+                .doctorStatus(doctor.getDoctorStatus())
                 .build();
     }
 }
