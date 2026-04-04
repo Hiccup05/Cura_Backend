c087ab8b62e0c1e7a1d667cb0fc33ecd89d5a940
Hiccup05 <paudelbipin05Z@gmail.com>
2026-04-04 09:06:51 +0545

    add get sum of payment by status

--- src/main/java/com/hiccup/cura/repository/PaymentRepository.java
+++ src/main/java/com/hiccup/cura/repository/PaymentRepository.java
@@ -3,8 +3,11 @@ package com.hiccup.cura.repository;
 import com.hiccup.cura.enums.PaymentStatus;
 import com.hiccup.cura.model.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @Repository
@@ -12,4 +15,7 @@ public interface PaymentRepository extends JpaRepository<Payment, Long> {
     Payment findByAppointment_IdAndPaymentStatusNotIn(Long appointmentId, List<PaymentStatus> paymentStatus);
 
     Payment findByPidx(String pidx);
+
+    @Query("SELECT COALESCE(SUM(p.amount)) FROM Payment p where p.paymentStatus= :paymentStatus")
+    BigDecimal sumAmountByPaymentStatus(@Param("paymentStatus") PaymentStatus paymentStatus);
 }
