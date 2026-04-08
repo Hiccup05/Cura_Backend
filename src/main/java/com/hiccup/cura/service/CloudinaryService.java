d10cea30bfef001082652e4256cd5dac0aa022a4
Hiccup05 <paudelbipin05Z@gmail.com>
2026-04-08 15:35:15 +0545

    create cloudinary service to add user profile picture

---
+++ src/main/java/com/hiccup/cura/service/CloudinaryService.java
@@ -0,0 +1,31 @@
+package com.hiccup.cura.service;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class CloudinaryService {
+    private final Cloudinary cloudinary;
+
+    public String uploadUserProfile(MultipartFile file, String publicId) throws IOException {
+        Map upload = cloudinary.uploader().upload(
+                file.getBytes(), ObjectUtils.asMap(
+                        "folder", "cura/users",
+                        "public_id", publicId,
+                        "overwrite", true
+                )
+        );
+        return upload.get("secure_url").toString();
+    }
+
+    public void delete(String publicId) throws IOException {
+        cloudinary.uploader().destroy(publicId, ObjectUtils.emptyMap());
+    }
+}
