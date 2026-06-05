07ea538ad2ca0e045f91d3e6218995c723d496b9
Hiccup05 <paudelbipin05@gmail.com>
2026-06-05 18:26:29 +0545

    paginate doctor repository

--- src/main/java/com/hiccup/cura/repository/DoctorRepository.java
+++ src/main/java/com/hiccup/cura/repository/DoctorRepository.java
@@ -2,6 +2,8 @@ package com.hiccup.cura.repository;
 
 import com.hiccup.cura.enums.DoctorStatus;
 import com.hiccup.cura.model.DoctorProfile;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -16,7 +18,7 @@ public interface DoctorRepository extends JpaRepository<DoctorProfile, Long> {
     boolean existsById(Long id);
 
     @Query("SELECT d FROM DoctorProfile d where d.doctorStatus IN :status")
-    List<DoctorProfile> getPublicDoctors(@Param("status") List<DoctorStatus> status);
+    Page<DoctorProfile> getPublicDoctors(@Param("status") List<DoctorStatus> status, Pageable pageable);
 
     @Query("SELECT d FROM DoctorProfile d where d.id=:id AND d.doctorStatus IN :status")
     Optional<DoctorProfile> getPublicDoctor(@Param("id") Long id, @Param("status") List<DoctorStatus> status);
@@ -24,5 +26,5 @@ public interface DoctorRepository extends JpaRepository<DoctorProfile, Long> {
     long countByDoctorStatusNot(DoctorStatus doctorStatus);
 
     @Query("SELECT d FROM DoctorProfile d WHERE LOWER(CONCAT(d.firstName, ' ', d.lastName)) LIKE LOWER(CONCAT('%', :name, '%'))")
-    List<DoctorProfile> searchByName(@Param("name") String name);
+    Page<DoctorProfile> searchByName(@Param("name") String name, Pageable pageable);
 }
