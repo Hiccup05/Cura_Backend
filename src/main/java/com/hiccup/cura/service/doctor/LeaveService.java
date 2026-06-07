5be37e394e2f1ca7ab7f4d349f51bf43d12b6d7d
Hiccup05 <paudelbipin05@gmail.com>
2026-06-07 19:32:46 +0545

    fix: add update doctor status to ON leave when creating leave

--- src/main/java/com/hiccup/cura/service/doctor/LeaveService.java
+++ src/main/java/com/hiccup/cura/service/doctor/LeaveService.java
@@ -2,12 +2,14 @@ package com.hiccup.cura.service.doctor;
 
 import com.hiccup.cura.dto.reqeust.LeaveRequestDto;
 import com.hiccup.cura.dto.response.LeaveResponseDto;
+import com.hiccup.cura.enums.DoctorStatus;
 import com.hiccup.cura.exception.custom.DuplicateEntryException;
 import com.hiccup.cura.exception.custom.ResourceNotFoundException;
 import com.hiccup.cura.model.DoctorLeave;
 import com.hiccup.cura.model.DoctorProfile;
 import com.hiccup.cura.repository.DoctorLeaveRepository;
 import com.hiccup.cura.repository.DoctorRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import java.util.List;
@@ -46,6 +48,7 @@ public class LeaveService {
                 doctorLeave.getDoctorProfile().getId());
     }
 
+    @Transactional
     public LeaveResponseDto createLeave(Long doctorId,LeaveRequestDto leaveRequestDto) {
         DoctorProfile doctorProfile = doctorRepository.findById(doctorId).orElseThrow(() -> new ResourceNotFoundException("Doctor cannot be found with id " + doctorId));
         if(doctorLeaveRepository.existsOverlappingLeave(doctorId,leaveRequestDto.getStartDate(),leaveRequestDto.getEndDate())) {
@@ -57,6 +60,7 @@ public class LeaveService {
         doctorLeave.setEndDate(leaveRequestDto.getEndDate());
         doctorLeave.setReason(leaveRequestDto.getReason());
         doctorLeave= doctorLeaveRepository.save(doctorLeave);
+        doctorProfile.setDoctorStatus(DoctorStatus.ON_LEAVE);
         return new LeaveResponseDto(doctorLeave.getId(), doctorLeave.getStartDate(), doctorLeave.getEndDate(), doctorLeave.getReason(),
                 doctorLeave.getDoctorProfile().getId());
     }
