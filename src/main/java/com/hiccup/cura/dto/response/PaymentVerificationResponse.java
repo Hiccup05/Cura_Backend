9d5619ec7f0c0fcd2a5947c9139eb92281dbd1b0
Hiccup05 <paudelbipin05@gmail.com>
2026-06-24 21:21:44 +0545

    create payment verification response  dto

--- src/main/java/com/hiccup/cura/dto/response/PaymentVerificationResponse.java
+++ src/main/java/com/hiccup/cura/dto/response/PaymentVerificationResponse.java
@@ -1,4 +1,18 @@
 package com.hiccup.cura.dto.response;
 
+import com.hiccup.cura.enums.PaymentStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class PaymentVerificationResponse {
+    private PaymentStatus status;
+    private String transactionUuid;
+    private String gatewayTransactionId;
+    private String message;
 }
