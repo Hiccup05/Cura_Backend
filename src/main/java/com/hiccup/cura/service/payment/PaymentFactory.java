57d978ae21da5965d36a1f04a0f5c6c440dce51e
Hiccup05 <paudelbipin05@gmail.com>
2026-06-24 21:23:09 +0545

    create payment factory

--- src/main/java/com/hiccup/cura/service/payment/PaymentFactory.java
+++ src/main/java/com/hiccup/cura/service/payment/PaymentFactory.java
@@ -1,4 +1,23 @@
 package com.hiccup.cura.service.payment;
 
+import com.hiccup.cura.enums.PaymentProvider;
+import com.hiccup.cura.exception.custom.PaymentProviderNotSupported;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Component
+@RequiredArgsConstructor
 public class PaymentFactory {
+    private final Map<String, PaymentStrategy> paymentStrategyMap;
+
+    public PaymentStrategy createPaymentStrategy(PaymentProvider paymentProvider){
+
+        if(paymentStrategyMap.containsKey(paymentProvider.getBeanName())){
+            return paymentStrategyMap.get(paymentProvider.getBeanName());
+        }else{
+            throw new PaymentProviderNotSupported(paymentProvider + " is not supported in Cura.");
+        }
+    }
 }
