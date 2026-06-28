5a8fa0be075841ea237f749f54d54ecea169db4f
Hiccup05 <paudelbipin05@gmail.com>
2026-06-28 19:08:36 +0545

    update object mapper

--- src/main/java/com/hiccup/cura/config/Utility.java
+++ src/main/java/com/hiccup/cura/config/Utility.java
@@ -1,12 +1,15 @@
 package com.hiccup.cura.config;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.web.reactive.function.client.WebClient;
-import tools.jackson.databind.ObjectMapper;
-import tools.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Configuration
 public class Utility {
@@ -30,9 +33,12 @@ public class Utility {
     }
 
     @Bean
+    @Primary
     public ObjectMapper objectMapper() {
-        return JsonMapper.builder()
-                .findAndAddModules()
-                .build();
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        return mapper;
     }
 }
