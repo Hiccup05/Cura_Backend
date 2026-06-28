3baa066f624039b0684049fe595adc5d2239a4b6
Hiccup05 <paudelbipin05@gmail.com>
2026-06-28 19:09:39 +0545

    create esewa initiate response dto

--- src/main/java/com/hiccup/cura/dto/response/EsewaInitiateResponseDto.java
+++ src/main/java/com/hiccup/cura/dto/response/EsewaInitiateResponseDto.java
@@ -1,4 +1,31 @@
 package com.hiccup.cura.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.hiccup.cura.enums.PaymentStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class EsewaInitiateResponseDto {
+    private String status;
+
+    private String signature;
+
+    @JsonProperty("transaction_code")
+    private String transactionCode;
+
+    @JsonProperty("total_amount")
+    private String totalAmount;
+
+    @JsonProperty("transaction_uuid")
+    private String transactionUuid;
+
+    @JsonProperty("product_code")
+    private String productCode;
+
+    @JsonProperty("signed_field_names")
+    private String signedFieldNames;
 }
