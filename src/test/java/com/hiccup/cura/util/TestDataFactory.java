66d89e6fea5cfb27d4ee26092582141ad318c4f4
Hiccup05 <paudelbipin05@gmail.com>
2026-07-01 18:46:20 +0545

    add utilities for unit test

--- src/test/java/com/hiccup/cura/util/TestDataFactory.java
+++ src/test/java/com/hiccup/cura/util/TestDataFactory.java
@@ -1,4 +1,41 @@
 package com.hiccup.cura.util;
 
+import com.hiccup.cura.enums.RoleType;
+import com.hiccup.cura.model.Role;
+import com.hiccup.cura.model.User;
+import net.datafaker.Faker;
+import java.util.Set;
+
 public class TestDataFactory {
+
+    private static final Faker faker = new Faker();
+
+    /**
+     * Generates a realistic User profile with a PATIENT role.
+     */
+    public static User createPatient() {
+        User user = new User();
+        user.setId(faker.number().randomNumber());
+        user.setEmail(faker.internet().emailAddress());
+        user.setActive(true);
+        // Setup the role exactly how your security model expects it
+        Role role = new Role();
+        role.setName(RoleType.PATIENT);
+        user.setRole(Set.of(role));
+
+        return user;
+    }
+
+    /**
+     * Generates a Doctor profile for testing authorization boundaries.
+     */
+    public static User createDoctor() {
+        User doctor = createPatient(); // Reuse the basic profile generation
+
+        Role role = new Role();
+        role.setName(RoleType.DOCTOR);
+        doctor.setRole(Set.of(role));
+
+        return doctor;
+    }
 }
