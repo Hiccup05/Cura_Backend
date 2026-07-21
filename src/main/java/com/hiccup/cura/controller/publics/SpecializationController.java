2a055b757c50ea38be39fbcac24081479d564a8e
Hiccup05 <paudelbipin05@gmail.com>
2026-07-21 18:20:28 +0545

    move endpoint from medical service controller

--- src/main/java/com/hiccup/cura/controller/publics/SpecializationController.java
+++ src/main/java/com/hiccup/cura/controller/publics/SpecializationController.java
@@ -1,6 +1,8 @@
-package com.hiccup.cura.controller;
+package com.hiccup.cura.controller.publics;
 
-import com.hiccup.cura.model.Specialization;
+import com.hiccup.cura.dto.response.MedicalServiceResponseDto;
+import com.hiccup.cura.dto.response.SpecializationDto;
+import com.hiccup.cura.service.MedicalServiceService;
 import com.hiccup.cura.service.SpecializationService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -13,19 +15,25 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("${api.prefix}/specialization")
+@RequestMapping("${api.prefix}/public/specializations")
 @RequiredArgsConstructor
 @Tag(name="Specialization", description = "About the specialization that doctors in CURA have.")
 public class SpecializationController {
     private final SpecializationService service;
+    private final MedicalServiceService medicalServiceService;
 
     @GetMapping
-    public ResponseEntity<List<Specialization>> getAll(){
+    public ResponseEntity<List<SpecializationDto>> getAll(){
         return ResponseEntity.ok(service.getAll());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Specialization> getOne(@PathVariable Long id){
+    public ResponseEntity<SpecializationDto> getOne(@PathVariable Long id){
         return ResponseEntity.ok(service.getById(id));
+    }
+
+    @GetMapping("/{id}/services")
+    public ResponseEntity<List<MedicalServiceResponseDto>> getSpecializationServices(@PathVariable Long id){
+        return ResponseEntity.ok(medicalServiceService.getActiveSpecializationServices(id));
     }
 }
